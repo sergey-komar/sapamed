df25f3c0602b7cdfb1154c5070a1e073f3972f33
sergey-komar <kursev81@yandex.ru>
2022-10-23 20:10:07 +0300

    page work

--- app/js/main.js
+++ app/js/main.js
@@ -4,4 +4,50 @@ $(function () {
     $(this).next().slideToggle(500); 
  });
 
+ $('.slider-reviews__block').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    appendArrows: '.slider-reviews__arrows',
+    prevArrow: '<button type="button" class="reviews__arrows-left">Previous</button>',
+    nextArrow: '<button type="button" class="reviews__arrows-right">Next</button>'
+ })
+
+ $('.slider-doctor__block').slick({
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  appendArrows: '.slider-doctor__arrows',
+  prevArrow: '<button type="button" class="doctor__arrows-left">Previous</button>',
+  nextArrow: '<button type="button" class="doctor__arrows-right">Next</button>'
+});
+$('.experts-doctor__block').slick({
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  appendArrows: '.experts-doctor__arrows',
+  prevArrow: '<button type="button" class="doctor__arrows-left">Previous</button>',
+  nextArrow: '<button type="button" class="doctor__arrows-right">Next</button>'
+});
+
+$('.slider-about__block').slick({
+  slidesToShow: 5,
+  slidesToScroll: 1,
+  appendArrows: '.slider-about__arrows',
+  prevArrow: '<button type="button" class="doctor__arrows-left">Previous</button>',
+  nextArrow: '<button type="button" class="doctor__arrows-right">Next</button>'
+});
+$('.slider-reviews__block-about').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  appendArrows: '.slider-reviews__arrows-about',
+  prevArrow: '<button type="button" class="doctor__arrows-left">Previous</button>',
+  nextArrow: '<button type="button" class="doctor__arrows-right">Next</button>'
+});
+
+const medicineBtn = document.querySelector('.medicine__btn');
+const medicineBottom = document.querySelector('.medicine__bottom');
+   
+  medicineBtn.addEventListener('click', ()=>{
+    medicineBottom.classList.toggle('hide');
+  })
+
+
 })
